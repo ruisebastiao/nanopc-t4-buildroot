0eb935de1ca706983ac5a11a07ad5dff4cd72389
Rui Sebastiao <ruisebastiao@gmail.com>
2019-09-13 18:14:51 +0100

    updated

--- overlays/common/base/etc/xdg/weston/wayland-env.sh
+++ overlays/common/base/etc/xdg/weston/wayland-env.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if test -z "${XDG_RUNTIME_DIR}"; then
-    export XDG_RUNTIME_DIR=/tmp/${UID}-runtime-dir
+    export XDG_RUNTIME_DIR=/tmp/runtime-root
     if ! test -d "${XDG_RUNTIME_DIR}"; then
         mkdir "${XDG_RUNTIME_DIR}"
         chmod 0700 "${XDG_RUNTIME_DIR}"
